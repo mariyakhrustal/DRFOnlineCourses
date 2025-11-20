dbdc5d82a7e419fbe7bf33a68df5e0d03b1c3cd2
Mariya Khrustaleva <mbotmanova@gmail.com>
2025-11-20 15:09:46 +0500

    Add nested lessons to CourseSerializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -4,8 +4,15 @@ from rest_framework.serializers import ModelSerializer
 from materials.models import Course, Lesson
 
 
+class LessonSerializer(ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
+
+
 class CourseSerializer(ModelSerializer):
     lessons_count = SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
 
     def get_lessons_count(self, course):
         return course.lessons.count()
@@ -13,10 +20,4 @@ class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
         fields = "__all__"
-        # fields = ("name", "description", "image", "lessons_count")
-
-
-class LessonSerializer(ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = "__all__"
+        # fields = ("name", "description", "image", "lessons_count", "lessons")
