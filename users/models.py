20230061a36b5e062504459f3e879364ef1a86ee
Mariya Khrustaleva <mbotmanova@gmail.com>
2025-11-20 12:36:45 +0500

    Add Payment model with relations to User, Course and Lesson in users

--- users/models.py
+++ users/models.py
@@ -36,3 +36,62 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+
+class Payment(models.Model):
+    PAYMENT_METHOD_CHOICES = [
+        ("cash", "Наличные"),
+        ("transfer", "Перевод на счёт"),
+    ]
+
+    users = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="payments",
+        verbose_name="Пользователь",
+        help_text="Укажите пользователя",
+    )
+    payment_date = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name="Дата платежа",
+        help_text="Укажите дату платежа",
+    )
+    course = models.ForeignKey(
+        "materials.Course",
+        on_delete=models.CASCADE,
+        related_name="payments",
+        blank=True,
+        null=True,
+        verbose_name="Оплаченный курс",
+        help_text="Укажите оплаченный курс",
+    )
+    lesson = models.ForeignKey(
+        "materials.Lesson",
+        on_delete=models.CASCADE,
+        related_name="payments",
+        blank=True,
+        null=True,
+        verbose_name="Оплаченный урок",
+        help_text="Укажите оплаченный урок",
+    )
+    payment_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        blank=True,
+        null=True,
+        verbose_name="Сумма платежа",
+        help_text="Укажите сумму платежа",
+    )
+    payment_method = models.CharField(
+        max_length=20,
+        choices=PAYMENT_METHOD_CHOICES,
+        blank=True,
+        null=True,
+        verbose_name="Способ оплаты",
+        help_text="Укажите способ оплаты",
+    )
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
