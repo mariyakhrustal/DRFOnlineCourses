34c7e3c3d120aef197a1f555067fec50895cc51d
Mariya Khrustaleva <mbotmanova@gmail.com>
2025-12-20 16:20:59 +0500

    Describe rights for moderators and add custom commant to create superuser

--- materials/views.py
+++ materials/views.py
@@ -9,11 +9,19 @@ from rest_framework.viewsets import ModelViewSet
 
 from materials.models import Course, Lesson
 from materials.serializers import CourseSerializer, LessonSerializer
+from users.permissions import IsModer
 
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    def get_permissions(self):
+        if self.action in ["create", "destroy"]:
+            self.permission_classes = (~IsModer,)
+        elif self.action in ["update", "retrieve"]:
+            self.permission_classes = (IsModer,)
+        return [permission() for permission in self.permission_classes]
 
 
 class LessonCreateAPIView(CreateAPIView):
